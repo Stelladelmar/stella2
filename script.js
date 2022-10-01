205d05c320172fbb0df1705c71e98cbe42f263a8
Stelladelmar <113214552+Stelladelmar@users.noreply.github.com>
2022-10-01 01:06:51 +0100

    Update script.js
    s

--- script.js
+++ script.js
@@ -93,3 +93,57 @@ document.getElementById('Carrot').addEventListener('click', (event) => {
   element_store6.appendChild(new_li7);
 
 });
+var productslist, totalprice;
+
+
+productslist = ['Menstrual cup', 'Metal straw', 'Reusable waxed sandwich wrap', 'Tote bag'];
+totalprice = [];
+
+
+document.getElementById('add1').addEventListener('click', (event) => {
+  let element_addeditems = document.getElementById('addeditems');
+  let new_li = document.createElement('li');
+  new_li.innerText = 'Menstrual cup price:£20';
+
+  element_addeditems.appendChild(new_li);
+  totalprice.unshift(20);
+  let element_total_price = document.getElementById('total_price');
+  element_total_price.innerText = totalprice.reduce((a,b) => a+b, 0);
+
+});
+
+document.getElementById('add2').addEventListener('click', (event) => {
+  let element_addeditems2 = document.getElementById('addeditems');
+  let new_li2 = document.createElement('li');
+  new_li2.innerText = 'Metal straw, pack x4 price:£15';
+
+  element_addeditems2.appendChild(new_li2);
+  totalprice.unshift(15);
+  let element_total_price2 = document.getElementById('total_price');
+  element_total_price2.innerText = totalprice.reduce((a,b) => a+b, 0);
+
+});
+
+document.getElementById('add3').addEventListener('click', (event) => {
+  let element_addeditems3 = document.getElementById('addeditems');
+  let new_li3 = document.createElement('li');
+  new_li3.innerText = 'Reusable waxed sandwich wrap price:£5';
+
+  element_addeditems3.appendChild(new_li3);
+  totalprice.unshift(5);
+  let element_total_price3 = document.getElementById('total_price');
+  element_total_price3.innerText = totalprice.reduce((a,b) => a+b, 0);
+
+});
+
+document.getElementById('add4').addEventListener('click', (event) => {
+  let element_addeditems4 = document.getElementById('addeditems');
+  let new_li4 = document.createElement('li');
+  new_li4.innerText = 'Tote bag price:£12';
+
+  element_addeditems4.appendChild(new_li4);
+  totalprice.unshift(12);
+  let element_total_price4 = document.getElementById('total_price');
+  element_total_price4.innerText = totalprice.reduce((a,b) => a+b, 0);
+
+});
